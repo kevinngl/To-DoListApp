174ee3b30c39a78573fded6819b80cec45b1792d
kevinngl <if320021@students.del.ac.id>
2023-07-13 16:06:14 +0700

    commit

---
+++ app/src/main/java/com/dicoding/todoapp/setting/SettingsActivity.kt
@@ -0,0 +1,59 @@
+package com.dicoding.todoapp.setting
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.SwitchPreference
+import androidx.work.PeriodicWorkRequest
+import androidx.work.WorkManager
+import com.dicoding.todoapp.R
+import com.dicoding.todoapp.notification.NotificationWorker
+import java.util.concurrent.TimeUnit
+
+class SettingsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.settings_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.settings, SettingsFragment())
+                .commit()
+        }
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    class SettingsFragment : PreferenceFragmentCompat() {
+        override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+            setPreferencesFromResource(R.xml.root_preferences, rootKey)
+
+            val prefNotification = findPreference<SwitchPreference>(getString(R.string.pref_key_notify))
+            prefNotification?.setOnPreferenceChangeListener { preference, newValue ->
+                val channelName = getString(R.string.notify_channel_name)
+                //TODO 13 : Schedule and cancel daily reminder using WorkManager with data channelName
+                val workManager = WorkManager.getInstance(requireContext())
+                val value = newValue as Boolean
+                if (value) {
+                    val notificationRequest = PeriodicWorkRequest.Builder(
+                        NotificationWorker::class.java,
+                        1, TimeUnit.DAYS
+                    ).build()
+                    workManager.enqueue(notificationRequest)
+                } else {
+                    // Cancel the previously scheduled work if notification is turned off
+                    workManager.cancelAllWorkByTag(NotificationWorker::class.java.simpleName)
+                }
+                true
+            }
+
+        }
+
+        private fun updateTheme(mode: Int): Boolean {
+            AppCompatDelegate.setDefaultNightMode(mode)
+            requireActivity().recreate()
+            return true
+        }
+    }
+}
